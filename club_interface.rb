3783286998b238619936aec47b782916d5b3675d
Jason Pelletier <jpelletier19@me.com>
2016-06-24 15:49:09 -0400

    further tests

--- club_interface.rb
+++ club_interface.rb
@@ -1,19 +1,22 @@
 require_relative "club_methods"
 
-yardage = 202.0
+yardage = 88.0
 aim = 0
 ball_in_stance = "normal"
 hands = "normal"
 
-#temp = temperature(61.0)
-#elev = elevation(-18.0, yardage)
-#windy = wind(21.0, yardage, "db")
-#lie = uneven_lie("d")
-#ruff = rough("n")
-#rain = rain("n")
-#bunker = bunker("n")
-#adjust = yardage + temp + elev + windy + lie
-#adjust_rough = adjust + (adjust * ruff)
-#puts "Adjusted yardage is: #{adjust_rough}"
+temp = temperature(76.0)
+elev = elevation(0, yardage)
+windy = wind(5.0, yardage, "f")
+lie = uneven_lie("n")
+ruff = rough("n")
+rain = rain("n")
+bunker = bunker("y")
+adjust = yardage + temp + elev + windy + lie + rain + bunker
+p adjust
+adjust_rough = adjust + (adjust * ruff)
+puts "Adjusted yardage is: #{adjust_rough}"
 
 #p bunker
+
+## PSUEDOCODE INTERFACE .....
