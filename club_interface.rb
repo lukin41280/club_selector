88508e9cc614459ba4601cd1a24b0f4ff8a5b613
Jason Pelletier <jpelletier19@me.com>
2016-06-25 15:56:30 -0400

    first part of UI, table created successfully

--- club_interface.rb
+++ club_interface.rb
@@ -1,21 +1,57 @@
+# program that will provide a golfer what club they should use depending upon 
+# multiple factors affecting their shot such as wind, slope, and temperature.
+# methods are created to adjust the yardage the golfer will have to account 
+# from their imputs.  the golfer will need to set up the distancces upfront
+# on how far they hit each club and this will be stored into a database titled 
+# as their name. once the questions regarding the shot conditions are answered,
+# the program will print the club to use based off the adjusted yardage. 
+
 require_relative "club_methods"
+require "SQLite3"
 
-yardage = 88.0
-aim = 0
-ball_in_stance = "normal"
-hands = "normal"
+## setting up golfer's clubs database
+puts "What is your name?"
+name = gets.chomp
+db = SQLite3::Database.new("#{name}.db")
+db.results_as_hash = true
 
-temp = temperature(76.0)
-elev = elevation(0, yardage)
-windy = wind(5.0, yardage, "f")
-lie = uneven_lie("n")
-ruff = rough("n")
-rain = rain("n")
-bunker = bunker("y")
-adjust = yardage + temp + elev + windy + lie + rain + bunker
-p adjust
-adjust_rough = adjust + (adjust * ruff)
-puts "Adjusted yardage is: #{adjust_rough}"
+## create table to hold user entered clubs
+clubs_table = <<-SQL
+		CREATE TABLE IF NOT EXISTS clubs (
+			name VARCHAR(255),
+			dist_yards INT
+		)
+	SQL
+db.execute(clubs_table)
+
+
+# club_enter = ""
+# until club_enter == "d"
+# 	puts "Enter a club or type 'd' if finished"
+# 	club = gets.chomp
+# 	puts "Enter the distance (only use yardage to the nearest 10s. ex - 130, 160, etc."
+# 	dist = gets.chomp
+
+	
+
+
+
+# yardage = 88.0
+# aim = 0
+# ball_in_stance = "normal"
+# hands = "normal"
+
+# temp = temperature(76.0)
+# elev = elevation(0, yardage)
+# windy = wind(5.0, yardage, "f")
+# lie = uneven_lie("n")
+# ruff = rough("n")
+# rain = rain("n")
+# bunker = bunker("y")
+# adjust = yardage + temp + elev + windy + lie + rain + bunker
+# p adjust
+# adjust_rough = adjust + (adjust * ruff)
+# puts "Adjusted yardage is: #{adjust_rough}"
 
 #p bunker
 
--- tester.rb
+++ tester.rb
@@ -1,50 +1,55 @@
 require 'sqlite3'
 
-puts "enter your name"
-name = gets.chomp
-puts "enter a club"
-club = gets.chomp
-puts "enter average distance"
-distance = gets.chomp
+puts "Are you already a member?"
+member = gets.chomp
+if member == "n"
+	puts "enter your name"
+	name = gets.chomp
+	puts "enter a club"
+	club = gets.chomp
+	puts "enter average distance"
+	distance = gets.chomp
 
-db = SQLite3::Database.new("#{name}.db")
-db.results_as_hash = true
+	$DB = SQLite3::Database.new("#{name}.db")
+	$DB.results_as_hash = true
 
-# create_club_table = <<-SQL
-# 	CREATE TABLE IF NOT EXISTS test(
-# 		low5 INT,
-# 		low4 INT,
-# 		low3 INT,
-# 		low2 INT,
-# 		low1 INT,
-# 		entry INT,
-# 		high1 INT,
-# 		high2 INT,
-# 		high3 INT,
-# 		high4 INT
-# 	)
-# SQL
+	# create_club_table = <<-SQL
+	# 	CREATE TABLE IF NOT EXISTS test(
+	# 		low5 INT,
+	# 		low4 INT,
+	# 		low3 INT,
+	# 		low2 INT,
+	# 		low1 INT,
+	# 		entry INT,
+	# 		high1 INT,
+	# 		high2 INT,
+	# 		high3 INT,
+	# 		high4 INT
+	# 	)
+	# SQL
 
 
-# db.execute(create_club_table)
+	# db.execute(create_club_table)
 
-small_table = <<-SQL
-	CREATE TABLE IF NOT EXISTS club (
-		name VARCHAR(255),
-		dist_yards INT
-	)
-SQL
+	small_table = <<-SQL
+		CREATE TABLE IF NOT EXISTS club (
+			name VARCHAR(255),
+			dist_yards INT
+		)
+	SQL
 
-db.execute(small_table)
-db.execute("INSERT INTO club (name, dist_yards) VALUES (?,?)", [club, distance])
-db.execute("INSERT INTO club (name, dist_yards) VALUES (?,?)", ["7i", 160])
-db.execute("INSERT INTO club (name, dist_yards) VALUES (?,?)", ["9i", 140])
+	db.execute(small_table)
+	db.execute("INSERT INTO club (name, dist_yards) VALUES (?,?)", [club, distance])
+	db.execute("INSERT INTO club (name, dist_yards) VALUES (?,?)", ["7i", 160])
+	db.execute("INSERT INTO club (name, dist_yards) VALUES (?,?)", ["9i", 140])
+end
 
+exist_db = SQLite3_load_extension("jay.db")
 yardage = 142.78
 yrd_adjuster = ((yardage/10).floor * 10)
 yrd_adjuster
 
-clubs = db.execute("SELECT * FROM club")
+clubs = exist_db.execute("SELECT * FROM club")
 #p clubs
 puts "here is test line"
 p clubs[0]["dist_yards"]
